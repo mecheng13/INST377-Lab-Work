12e35f9b649207c7aecf307623063392d73eaf34
mecheng13 <mcheng13@terpmail.umd.edu>
2022-02-25 20:24:24 -0500

    lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,42 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+function updateSlidePosition() {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+  updateSlidePosition();
+}
+
+document
+  .getElementById('carousel_button--next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
+
+document
+  .getElementById('carousel_button--prev')
+  .addEventListener('click', () => {
+    moveToPrevSlide();
+  });
